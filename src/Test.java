193f64aacf6205e1bba1d55cd9e3d60ff86055d4
Bogdan Liutikov <lbi2001@yandex.ru>
2024-02-14 20:44:49 +0300

    lab1

---
+++ src/Test.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] ns = new int[]{100, 500, 1_000, 5_000, 10_000, 50_000, 100_000};
+        for (int n : ns) {
+            System.out.println("Граф с количеством вершин: " + n);
+            performanceTest(n);
+            System.out.println();
+        }
+//        System.out.println(correctTest());
+    }
+
+    private static List<Integer>[] graphGenerate(int n) {
+        Random random = new Random();
+        List<Integer>[] g = new ArrayList[n];
+        for (int i = 0; i < n; i++)
+            g[i] = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int u = random.nextInt(i + 1);
+            for (int j = 0; j < 100; j++) {
+                int v = random.nextInt(n - i) + i;
+                g[u].add(v);
+            }
+        }
+        return g;
+    }
+
+    private static void performanceTest(int n) {
+        List<Integer>[] g = graphGenerate(n);
+        TopologicalSorting ts = new TopologicalSorting(g);
+
+        int avg_n = 100;
+        long[] times = new long[avg_n];
+        double[] mems = new double[avg_n];
+        Runtime runtime = Runtime.getRuntime();
+        for (int k = 0; k < avg_n; k++) {
+            long time_start = System.currentTimeMillis();
+            ts.topological_sort();
+            long time_end = System.currentTimeMillis();
+            double mem = (runtime.totalMemory() - runtime.freeMemory()) / 1048576.;
+            times[k] = time_end - time_start;
+            mems[k] = mem;
+        }
+
+        double time_avg = Arrays.stream(times).average().orElse(Double.NaN);
+        double mem_avg = Arrays.stream(mems).average().orElse(Double.NaN);
+
+        System.out.println("Время: " + time_avg);
+        System.out.println("Память: " + mem_avg);
+    }
+
+    private static boolean correctTest() {
+        int n = 5;
+        TopologicalSorting ts = new TopologicalSorting(n);
+        List<Integer>[] g = ts.g;
+        g[0].addAll(Arrays.asList(1, 3));
+        g[1].addAll(Arrays.asList(2, 3, 4));
+        g[2].addAll(Arrays.asList(4));
+//        g[3].addAll(Collections.emptyList());
+        g[4].addAll(Arrays.asList(3));
+
+        List<Integer> ans = ts.topological_sort();
+        System.out.println(ans);
+        return ans.equals(Arrays.asList(0, 1, 2, 4, 3));
+    }
+}
---
+++ src/TopologicalSorting.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TopologicalSorting {
+    int n;
+    List<Integer>[] g;
+    boolean[] used;
+    ArrayList<Integer> ans;
+
+    public TopologicalSorting(int n) {
+        this.n = n;
+        g = new ArrayList[n];
+        for (int i = 0; i < n; i++)
+            g[i] = new ArrayList<>();
+        used = new boolean[n];
+        ans = new ArrayList<>();
+    }
+
+    public TopologicalSorting(List<Integer>[] graph) {
+        g = graph;
+        this.n = g.length;
+        used = new boolean[this.n];
+        ans = new ArrayList<>();
+    }
+
+    void dfs(int v) {
+        used[v] = true;
+        for (int i = 0; i < g[v].size(); ++i) {
+            int to = g[v].get(i);
+            if (!used[to])
+                dfs(to);
+        }
+        ans.add(v);
+    }
+
+    List<Integer> topological_sort() {
+        for (int i = 0; i < n; ++i)
+            if (!used[i])
+                dfs(i);
+        Collections.reverse((ans));
+        return ans;
+    }
+}
+
